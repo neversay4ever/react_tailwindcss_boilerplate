1c377e631d6ef561a5ac2b7b9866979d9ad4167d
neversay4ever <neversay4ever@163.com>
2020-04-13 07:26:51 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './components/Map';
+import Counter from './components/Counter'
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Map />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +22,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <Counter />
+
     </div>
   );
 }
